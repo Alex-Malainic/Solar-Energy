562a960cc6220eb02efce9ff94e939eb0c19c835
Alex Malainic <50654936+Alex-Malainic@users.noreply.github.com>
2023-06-29 13:15:54 +0300

    Update Main_Functions.py

--- Main_Functions.py
+++ Main_Functions.py
@@ -5,7 +5,6 @@ import numpy as np
 from sklearn.preprocessing import OrdinalEncoder, StandardScaler
 from sklearn.model_selection import GridSearchCV
 from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
-from tensorflow import keras
 from scikeras.wrappers import KerasRegressor
 import matplotlib.pyplot as plt
 import seaborn as sns
